8da7dec48601d46c601cae0dc812a51c1a738a00
sergpapa <>
2024-01-11 12:47:28 +0000

    added validation to check if input is 6 values

--- main.py
+++ main.py
@@ -25,7 +25,24 @@ def get_sales_data():
     print("Data should be six numbers, seperated by commas")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data you provided: {data_str}")
+    data_str = input("Enter your data here: ")  
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers
+    Raises ValueErorr if strings cannot be converted into int,
+    or if there aren't exaclty 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
