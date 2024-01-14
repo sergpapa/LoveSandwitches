486aa1a752d8c1e57c61e241e68f66e7532a043a
sergpapa <>
2024-01-14 13:05:04 +0000

    Collect sales data for last 5 entries

--- main.py
+++ main.py
@@ -87,6 +87,22 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully.\n")
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns 
+
+
 def main():
     """
     Run all program functions
@@ -96,6 +112,7 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
 
 
 print("Welcome to Love Sandwitches Data Automation")
